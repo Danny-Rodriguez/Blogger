6a4bdfaaa13a808d6e9c86a42d63c4a89dab67be
Danny Rodriguez <rod.dr.dan@gmail.com>
2022-05-23 21:31:31 -0700

    ready for back-end

---
+++ app/components/Page.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react"
+import Container from "./Container"
+
+function Page(props) {
+  useEffect(() => {
+    document.title = `${props.title} | Blogger`
+    window.scrollTo(0, 0)
+  }, []) //how you tell React to only run something the first time it's rendered
+
+  return <Container wide={props.wide}>{props.children}</Container>
+}
+
+export default Page
