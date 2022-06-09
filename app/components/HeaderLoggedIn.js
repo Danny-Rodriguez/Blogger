57fea5cba7e4734fc8167a2f45688ecbe1ba270c
Danny Rodriguez <rod.dr.dan@gmail.com>
2022-06-08 22:22:15 -0700

    added flash messages

---
+++ app/components/HeaderLoggedIn.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react"
+import { Link } from "react-router-dom"
+
+function HeaderLoggedIn(props) {
+  function handleLogout() {
+    props.setLoggedIn(false)
+    localStorage.removeItem("BloggerToken")
+    localStorage.removeItem("BloggerUsername")
+    localStorage.removeItem("BloggerAvatar")
+  }
+
+  // "https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
+
+  return (
+    <div className="flex-row my-3 my-md-0">
+      <a href="#" className="text-white mr-2 header-search-icon">
+        <i className="fas fa-search"></i>
+      </a>
+      <span className="mr-2 header-chat-icon text-white">
+        <i className="fas fa-comment"></i>
+        <span className="chat-count-badge text-white"> </span>
+      </span>
+      <a href="#" className="mr-2">
+        <img className="small-header-avatar" src={localStorage.getItem("BloggerAvatar")} />
+      </a>
+      <Link className="btn btn-sm btn-success mr-2" to="/create-post">
+        Create Post
+      </Link>
+      <button onClick={handleLogout} className="btn btn-sm btn-secondary">
+        Sign Out
+      </button>
+    </div>
+  )
+}
+
+export default HeaderLoggedIn
